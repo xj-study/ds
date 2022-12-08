229d38d7919c245c4eb7de131752ca093320c290
Jun.Xie8 <Jun.Xie8@lotuscars.com.cn>
2022-12-08 11:04:34 +0800

    fix

---
+++ packages/p2/index-browser.js
@@ -0,0 +1,9 @@
+(() => {
+  // ../p1/index.js
+  function add(a, b) {
+    return a + b;
+  }
+
+  // index.js
+  console.log("add", add(1, 2));
+})();
---
+++ packages/p2/index.js
@@ -0,0 +1,3 @@
+import { add } from "@demo/p1";
+
+console.log("add", add(1, 2));
